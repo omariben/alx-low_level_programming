5bb89978996accc15238753194ac8db330274866
omariben <benmsungu65@gmail.com>
2023-06-15 15:30:22 -0700

    size not all

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,19 @@
 
 /**
  * main - Size is not grandeur, and territory does not make a nation
- *
  * Return: 0
  */
 int main(void)
 {
-printf("size of char: %d byte(s)\n", size of(char));
-printf("size of int: %d byte(s)\n", size of(int));
-printf("size of long int: %d byte(s)\n", size of(long int));
-printf("size of long long int: %d byte(s)\n", size of(long long int));
-printf("size of float: %d byte(s)\n", size of(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+printf("size of char: %lu byte(s)\n", size of(a));
+printf("size of int: %lu byte(s)\n", size of(b));
+printf("size of long int: %lu byte(s)\n", size of(c));
+printf("size of long long int: %lu byte(s)\n", size of(d));
+printf("size of float: %lu byte(s)\n", size of(e));
 return (0);
 }
