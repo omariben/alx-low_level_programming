574ea66a603e719292c466c26b81e4bfbe5c8bf8
omariben <benmsungu65@gmail.com>
2023-06-23 01:38:57 -0700

    more functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,20 +5,17 @@
  * Description: prints numbers 0 to 9 excluding 2 and 4
  * Return: numbers 0 to 9
 */
-void print_most_numbers(void)
-{
-	int x = 0;
-
-	for (x <= 9; x++)
-	{
-	if (x == 2 || x == 4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(x + '0');
-	}
-	_putchar ("\n");
-	}
+void printNumbers() {
+    for (int i = 0; i <= 9; i++) {
+        if (i != 2 && i != 4) {
+            _putchar(i + '0');
+            _putchar(' ');
+        }
+    }
+    _putchar('\n');
 }
+
+int main() {
+    printNumbers();
+    return 0;
+
