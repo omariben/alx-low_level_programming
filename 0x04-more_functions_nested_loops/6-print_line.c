6c811079edbec7a9d6e9996c4e62ca7adc06491b
omariben <benmsungu65@gmail.com>
2023-06-24 13:49:13 -0700

    underscore drawing

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,25 @@
 #include "main.h"
+int _putchar(char c);
+
 /**
- *print_line - Draw a straight line
- *@n: number of lines to draw
+ * print_line - Prints a line of underscores based on parameter n
+ * @n: number of times to draw the underscore
  * Return: Empty
- */
+*/
 void print_line(int n)
 {
-	int x;
+int x;
 
-	if (n <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	for (x < 0; x < n; x++)
-	{
-	_putchar(95);
-	}
-	_putchar('\n');
-	}
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
 }
