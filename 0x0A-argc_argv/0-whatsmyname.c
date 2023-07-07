681f95b70ca4fb90ddd23ebcfe7bb4f98b54ffd3
omariben <benmsungu65@gmail.com>
2023-07-07 12:31:19 -0700

    my new name task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,13 +4,12 @@
  * main - prints the name
  * @argc: Argument count
  * @argv: Argument vector
- * 
  * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-    (void) argc;
-    printf("%s\n", argv[0]);
+(void) argc;
+printf("%s\n", argv[0]);
 
-    return 0;
+return (0);
 }
