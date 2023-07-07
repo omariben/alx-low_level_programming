e7a3d3cebdb6a1517546bac67264de3bcdd19b69
omariben <benmsungu65@gmail.com>
2023-07-07 11:46:26 -0700

    prints all arguements it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguements it receives
+ * @argv: Arguement vector
+ * @argc: arguement count
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+
+return (0);
+}
