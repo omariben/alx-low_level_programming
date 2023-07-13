97d697ed6f82aeef75ade1a071e0a485e55078cb
omariben <benmsungu65@gmail.com>
2023-07-13 09:58:05 -0700

    memory allocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,31 +15,42 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-size_t len1;
-size_t len2;
-char *result;
+unsigned int i = 0, j = 0, k = 0, l = 0;
+char *str;
 
 if (s1 == NULL)
-{
-s1 = " ";
-}
-if (s2 == NULL)
-{
-s2 = " ";
-}
-len1 = strlen(s1);
-len2 = strlen(s2);
-if (n >= len2)
-{
-n = len2;
-}
-result = malloc(len1 + n + 1);
-if (result == NULL)
-{
-return (NULL);
-}
-strcpy(result, s1);
-strncat(result, s2, n);
-return (result);
-}
 
+s1 = " ";
+
+if (s2 == NULL)
+
+s2 = " ";
+while (s1[i])
+i++;
+while (s2[k])
+k++;
+
+if (n >= k)
+
+l = i + k;
+else
+l = i + n;
+str = malloc(sizeof(char) * l + n);
+if (str == NULL)
+
+return (NULL);
+k = 0;
+while (j < l)
+{
+if (j <= i)
+str[j] = s1[j];
+if (j >= i)
+{
+str[j] = s2[k];
+k++;
+}
+j++;
+}
+str[j] = '\0';
+return (str);
+}
