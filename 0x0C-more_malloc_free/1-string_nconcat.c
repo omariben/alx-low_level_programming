294ac591e6a9c9a3f7b937311d399bd2ad7d0c8a
omariben <benmsungu65@gmail.com>
2023-07-14 02:19:44 -0700

    concatenating two string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,25 +19,19 @@ unsigned int i = 0, j = 0, k = 0, l = 0;
 char *str;
 
 if (s1 == NULL)
-
 s1 = " ";
-
 if (s2 == NULL)
-
 s2 = " ";
 while (s1[i])
 i++;
 while (s2[k])
 k++;
-
 if (n >= k)
-
 l = i + k;
 else
 l = i + n;
 str = malloc(sizeof(char) * l + n);
 if (str == NULL)
-
 return (NULL);
 k = 0;
 while (j < l)
